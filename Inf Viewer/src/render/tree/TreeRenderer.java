2342fa870fb4ccc1b42413d8f8b39a5846861202
Ratomir Vukadin <Ratomir@live.com>
2017-07-15 09:19:32 +0200

    Tree render imports.

--- Inf Viewer/src/render/tree/TreeRenderer.java
+++ Inf Viewer/src/render/tree/TreeRenderer.java
@@ -3,7 +3,6 @@ package render.tree;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
 
 import javax.swing.Box;
 import javax.swing.Icon;
